ff0a38ef3a59a6900cb9beaa9954938511762780
josep333 <jose.revello20@gmail.com>
2019-11-02 12:45:06 -0300

    se creo la clase conexion

---
+++ src/java/ws/Conexion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Asus
+ */
+public class Conexion {
+    private static String servidor="jdbc:oracle:thin:@mycm.cl:4921:XE [siglo21 en SIGLO21]"; 
+    private static String usuario ="siglo21";
+    private static String contrasenia="siglo21";
+    public static String driver="oracle.jdbc.OracleDriver";
+    private static Connection conexion;
+
+    public Conexion() {
+        try {
+            Class.forName(driver);
+            conexion=DriverManager.getConnection(servidor, usuario, contrasenia);
+            System.out.println("conexion exitosa");
+            
+        } catch (ClassNotFoundException|SQLException e) {
+            System.out.println("conexion fallida");
+        }
+        
+    }
+    public Connection getConnection(){
+    return conexion;
+    }
+    
+    
+}
