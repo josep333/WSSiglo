a2db374b0dd0a02931d02e1f5a01243d12ebd743
josep333 <jose.revello20@gmail.com>
2019-11-11 22:26:40 -0300

    servicio full

--- src/java/ws/Service1.java
+++ src/java/ws/Service1.java
@@ -18,32 +18,30 @@ import javax.jws.WebParam;
  *
  * @author Asus
  */
-@WebService(serviceName = "NewWebService")
-public class NewWebService {
+@WebService(serviceName = "Service1")
+public class Service1 {
 
 //se instancia la conexion se llama al statemen     
     Conexion cone = new Conexion();
     Connection con = cone.getConnection();
     Statement st;
-    ResultSet rs,proce;
+    ResultSet rs, proce;
 
     /**
      * Web service operation
      */
     @WebMethod(operationName = "WSSII")
     public int WSSII(@WebParam(name = "id") int id) {
-        String sql = "call sp_boleta(" + id + ")";
+        String sql = "call sp_boleta(?)";
         try {
-            st = con.createStatement();
-            rs = st.executeQuery(sql);
+            CallableStatement statement = con.prepareCall(sql);
+            statement.setInt(id, id);
+            statement.execute();
             con.close();
-            st.close();
-            rs.close();
             return 1;
         } catch (SQLException e) {
             return 0;
         }
-
     }
 
     /**
@@ -51,36 +49,34 @@ public class NewWebService {
      */
     @WebMethod(operationName = "WStransferencia")
     public int WStransferencia(@WebParam(name = "run") String run, @WebParam(name = "pass") String pass, @WebParam(name = "montoPago") int montoPago) {
-        
-     String valor = "select t.MONTO from transferencia t where t.RUT='" + run + "' and t.PASS='" + pass + "'";
-     
+
+        String valor = "select t.MONTO from transferencia t where t.RUT='" + run + "' and t.PASS='" + pass + "'";
+
         try {
-            st=con.createStatement();
-            rs=st.executeQuery(valor);
+            st = con.createStatement();
+            rs = st.executeQuery(valor);
             int res = 0;
-            while(rs.next())
-            {
+            while (rs.next()) {
                 res = rs.getInt(1);
-              //  System.err.println(res);
+                //  System.err.println(res);
             }
-            int monto = res-montoPago;
+            int monto = res - montoPago;
 //            String procedimiento="call  SP_TRANSFERENCIA ('"+run+"','"+pass+"',"+monto+")";
 //            proce=st.executeQuery(procedimiento);
 //            System.out.println("señor jesucristo"+monto);
-            
-            String query = "{call  SP_TRANSFERENCIA(?,?,?)}"; 
-            CallableStatement statement = con.prepareCall(query);  
+
+            String query = "{call  SP_TRANSFERENCIA(?,?,?)}";
+            CallableStatement statement = con.prepareCall(query);
             statement.setString(1, run);
             statement.setString(2, pass);
             statement.setInt(3, monto);
-            statement.execute(); 
-            
+            statement.execute();
+
             return monto;
         } catch (Exception e) {
-            System.out.println(e.getMessage()+" se mamo");
+            System.out.println(e.getMessage() + " se mamo");
             return 0;
         }
-
 
     }
 
